fb91ac02ec8444869b3b8b30477009b3556eff7f
Tom <thomas.legrand@axellience.com>
2014-02-24 19:08:13 +0100

    formated using intellij

--- codenvy-ext-wso2-core/src/main/java/com/codenvy/ide/ext/wso2/client/editor/graphical/GraphicEditorView.java
+++ codenvy-ext-wso2-core/src/main/java/com/codenvy/ide/ext/wso2/client/editor/graphical/GraphicEditorView.java
@@ -21,7 +21,6 @@ import com.codenvy.ide.api.mvp.View;
 import com.genmymodel.ecoreonline.graphic.Diagram;
 import com.google.inject.ImplementedBy;
 import com.google.web.bindery.event.shared.EventBus;
-
 import org.genmymodel.gmmf.propertypanel.PropertyPresenter;
 import org.wso2.developerstudio.eclipse.gmf.esb.EsbSequence;
 
@@ -38,8 +37,9 @@ public interface GraphicEditorView extends View<GraphicEditorView.ActionDelegate
     }
 
     void initModelingWidgets(EsbSequence sequence, Diagram diagram);
+
     void addPropertyForm(PropertyPresenter... forms);
-    
+
     EventBus getDiagramEventBus();
 
 }
