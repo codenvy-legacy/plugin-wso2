3609acbc5b5868b5b14973e279446533fb935d5b
Tom <thomas.legrand@axellience.com>
2014-03-06 18:20:32 +0100

    added ValiditionNotifierImpl to GraphicEditorViewImpl constructor

--- codenvy-ext-wso2-core/src/main/java/com/codenvy/ide/ext/wso2/client/editor/graphical/GraphicEditorViewImpl.java
+++ codenvy-ext-wso2-core/src/main/java/com/codenvy/ide/ext/wso2/client/editor/graphical/GraphicEditorViewImpl.java
@@ -65,10 +65,11 @@ public class GraphicEditorViewImpl extends Composite implements GraphicEditorVie
     private EventBus diagramEventBus;
 
     @Inject
-    public GraphicEditorViewImpl(WSO2Resources resources, EventBus globalBus) {
+    public GraphicEditorViewImpl(WSO2Resources resources, EventBus globalBus, ValiditionNotifierImpl validationNotifier) {
         this.res = resources;
         this.globalBus = globalBus;
-
+       // this.validator = validator;
+        
         // we use a local bus for the communication between the toolbar and the widgets
         this.diagramEventBus = new SimpleEventBus();
 
@@ -76,7 +77,7 @@ public class GraphicEditorViewImpl extends Composite implements GraphicEditorVie
         this.modelWidget = new ModelWidget(diagramEventBus);
 
         // the ESB-specific toolbar
-        this.toolbar = new ESBDiagramToolbar(modelWidget, this.globalBus, res.wso2Style(), res);
+        this.toolbar = new ESBDiagramToolbar(modelWidget, validationNotifier, this.globalBus, res.wso2Style(), res);
 
         initWidget(binder.createAndBindUi(this));
     }
