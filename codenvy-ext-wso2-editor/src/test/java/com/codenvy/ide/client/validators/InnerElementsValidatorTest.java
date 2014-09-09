c74d4a09e97a80e267486d9127d6d6c47e47c90b
Andrey Plotnikov <aplotnikov@codenvy.com>
2014-09-09 17:56:55 +0300

    Merge

--- codenvy-ext-wso2-editor/src/test/java/com/codenvy/ide/client/validators/InnerElementsValidatorTest.java
+++ codenvy-ext-wso2-editor/src/test/java/com/codenvy/ide/client/validators/InnerElementsValidatorTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2014 Codenvy, S.A.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache  License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.codenvy.ide.client.validators;
 
 import org.junit.Before;
